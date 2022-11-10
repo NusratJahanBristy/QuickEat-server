7fdfcaaab459f5bdee7ff92ec05ed2eef9e20ba6
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-11-10 17:37:00 +0600

    reviews data get

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Port = process.env.Port || 5000;
 app.use(cors());
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
     res.send("quick eat  server is running");
 });
@@ -21,15 +22,22 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('quickEat').collection('services');
-      
-
+        const reviewCollection = client.db('quickEat').collection('reviews');
+        
         app.get('/services', async (req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+            const count=parseInt(req.query.count);
+            const cursor = serviceCollection.find({});
+            const services = await cursor.limit(count).toArray();
             res.send(services);
             console.log(services)
         });
+        // app.get('/services', async (req, res) => {
+        //     const query = {}
+        //     const cursor = serviceCollection.find(query);
+        //     const services = await cursor.toArray();
+        //     res.send(services);
+        //     console.log(services)
+        // });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
@@ -39,46 +47,36 @@ async function run() {
         });
 
 
-        // orders api
-        // app.get('/orders', async (req, res) => {
-        //     let query = {};
+        //review api
+        app.get('/reviews', async (req, res) => {
+            let query = {};
 
-        //     if (req.query.email) {
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }
+           
+             // if (req.query.service_id) {
+            //     query = {
+            //         service_id: req.query.service_id
+            //     }
+            // }
+             // if (req.query.email) {
+            //     query = {
+            //         email: req.query.email
+            //     }
+            // }
 
-        //     const cursor = orderCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // });
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
-        // app.post('/orders', async (req, res) => {
-        //     const order = req.body;
-        //     const result = await orderCollection.insertOne(order);
-        //     res.send(result);
-        // });
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
 
-        // app.patch('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const status = req.body.status
-        //     const query = { _id: ObjectId(id) }
-        //     const updatedDoc = {
-        //         $set:{
-        //             status: status
-        //         }
-        //     }
-        //     const result = await orderCollection.updateOne(query, updatedDoc);
-        //     res.send(result);
-        // })
 
-        // app.delete('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await orderCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+
+
 
 
     }
