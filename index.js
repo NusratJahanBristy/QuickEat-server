9d3a5293a1fc7ff776c02cb71951a4b0f92348bb
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-11-10 18:17:51 +0600

    delete crud operation added for review delete

--- index.js
+++ index.js
@@ -79,6 +79,27 @@ async function run() {
 
 
 
+        // app.patch('/orders/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const status = req.body.status
+        //     const query = { _id: ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set:{
+        //             status: status
+        //         }
+        //     }
+        //     const result = await orderCollection.updateOne(query, updatedDoc);
+        //     res.send(result);
+        // })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
     }
     finally {
 
